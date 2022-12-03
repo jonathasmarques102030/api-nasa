f0e11d6ce34f53d71134ac7334e58a7f02804753
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2022-12-03 12:15:23 -0300

    reformulado

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
-const express = require("express");
-const app = express()
-const port = "3000"
+const linkApiKey = "https://api.nasa.gov/planetary/apod?api_key=fAG7p0bAgf57XnC85DcpZWn9wcGP8OR0AoOuJhLS"
 
-app.set("view engine","ejs");
 
-app.use(express.static(__dirname + "/public"));
+function imprimeData() {
+    const data = document.getElementById("date").value;
+    const linkApiKey = `https://api.nasa.gov/planetary/apod?api_key=cqTvDkhLIHngFex32zzx8SuZtC7AdFeihkPKIeZV&date=${data}`
+    console.log(linkApiKey)
+    console.log(data)
+}
 
-app.get("/", (req, res) => {
-    res.render("index")
-})
+const imprimeDados = async(dados) => {
+    const data = document.getElementById("date").value;
+    const linkApiKey = await fetch(`https://api.nasa.gov/planetary/apod?api_key=cqTvDkhLIHngFex32zzx8SuZtC7AdFeihkPKIeZV&date=${data}`)
+    .then(res => res.json())
+    .then(res => {
+        var elemento = document.getElementById('descritivo');
+            elemento.innerHTML = `<p>${res.explanation}</p>`
+    })
+    imprimeData()
+}
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta: ${port}`)
-})
+const ImprimeImagem = async (img) => {
+
+    const data = document.getElementById("date").value
+    const linkApiKey = `https://api.nasa.gov/planetary/apod?api_key=cqTvDkhLIHngFex32zzx8SuZtC7AdFeihkPKIeZV&date=${data}`
+    const apiKey = await fetch(`https://api.nasa.gov/planetary/apod?api_key=cqTvDkhLIHngFex32zzx8SuZtC7AdFeihkPKIeZV&date=${data}`)
+    .then(res => res.json())
+    .then(res =>{
+        let body = document.body.style.backgroundImage = `url(${res.hdurl})`;
+        document.body.style.backgroundPositionX = "center"
+        document.body.style.backgroundPositionY = "0%"
+    })
+    console.log(linkApiKey)
+    console.log(apiKey)
+}
+
+function imagem(){
+    ImprimeImagem()
+}
+
+function dados(){
+    abrirModalSobre()
+    imprimeDados()
+}
+
+function abrirModalSobre() {
+    let modalSobre = document.getElementById("modal-sobre");
+
+    modalSobre.style.display = 'block';
+};
+
+function fecharModalSobre() {
+    let modalSobre = document.getElementById("modal-sobre");
+
+    modalSobre.style.display = 'none';
+};
+
+
+document.addEventListener("click", (e) => {
+    e.preventDefault()
+});
